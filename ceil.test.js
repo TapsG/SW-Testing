26d69df2f1859d8e10547a35e0e280c1d282c378
TapsG <50074292+TapsG@users.noreply.github.com>
2021-12-01 20:10:08 +0200

    Update ceil.test.js

--- ceil.test.js
+++ ceil.test.js
@@ -5,10 +5,10 @@ var expect = chai.expect
 describe('ceil()', () => {
     it('input numbers 5030 and -2, result should be number and equal to 51o0', () => {
         expect(ceil(5030,-22)).to.be.a('number');
-        expect(ceil(5030,-2)).to.equal(5100);
+        expect(ceil(5030,-2)).to.equal(51o0);
     });
     it('input numbers 7,o03 and 2, result should be number and equal to 7,01', () => {
-        expect(ceil(7,003,2)).to.be.a('number');
-        expect(ceil(7,003,2)).to.equal(7,01);
+        expect(ceil(7,o03,2)).to.be.a('number');
+        expect(ceil(7,o03,2)).to.equal(7,01);
     });
 });
