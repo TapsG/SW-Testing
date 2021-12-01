3168e5a73354177852b899a6c7f318a472c7fc67
laril <lari.lantonen@tuni.fi>
2021-12-01 17:44:48 +0200

    test 8 done

--- upperFirst.test.js
+++ upperFirst.test.js
@@ -1,16 +1,16 @@
 import chai from 'chai';
-import toString from './src/upperFirst.js';
+import upperFirst from './src/upperFirst.js';
 var expect = chai.expect
 
 describe('upperFirst()', () => {
-    it('input number 3, result should be string and equal to "3"', () => {
-        expect(toString(3)).to.be.string;
-        expect(toString(3)).to.equal("3");
+    it('"hot wings", result should be string and equal to "Hot wings"', () => {
+        expect(upperFirst("hot wings")).to.be.string;
+        expect(upperFirst("hot wings")).to.equal("Hot wings");
     });
 
-    it('inputarray [2,3,4], result should be string and equal to "2,3,4"', () => {
-        expect(toString([2,3,4])).to.be.string;
-        expect(toString([2,3,4])).to.equal("2,3,4");
+    it('"Hot wings", result should be string and equal to "Hot wings"', () => {
+        expect(upperFirst("Hot wings")).to.be.string;
+        expect(upperFirst("Hot wings")).to.equal("Hot wings");
     });
 
 });
