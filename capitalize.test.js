5e65d98019d7e58f56f7d3dcbc04274e9f0c1917
TapsG <50074292+TapsG@users.noreply.github.com>
2021-12-01 19:52:11 +0200

    Update capitalize.test.js

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,11 +3,11 @@ import add from './src/capitalize.js';
 var expect = chai.expect
 
 describe('capitalize()', () => {
-    it('input string "kEBAB", result should be string and equal to "Kebab", () => {
+    it('input string "kEBAB", result should be string and equal to "Kebab"', () => {
         expect(capitalize("kEBAB")).to.be.string;
         expect(capitalize("kEBAB")).to.equal("Kebab");
     });
-    it('input string "KEBAB", result should be string and equal to "Kebab", () => {
+    it('input string "KEBAB", result should be string and equal to "Kebab"', () => {
         expect(capitalize("KEBAB")).to.be.string;
         expect(capitalize("KEBAB")).to.equal("Kebab");
     });
