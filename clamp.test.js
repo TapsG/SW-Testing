4852166d0c3ab106142b0c2be470c64dc99fdaf4
TapsG <50074292+TapsG@users.noreply.github.com>
2021-12-02 11:20:39 +0200

    Update clamp.test.js

--- clamp.test.js
+++ clamp.test.js
@@ -3,16 +3,16 @@ import clamp from './src/clamp.js';
 var expect = chai.expect
 
 describe('clamp()', () => {
-    it('input numbers 4, -4 and 7, result should be number and equal to 4', () => {
-        expect(clamp(4,-4,7)).to.be.a('number');
-        expect(clamp(4,-4,7)).to.equal(4);
+    it('input numbers 7, 4 and -4, result should be number and equal to 4', () => {
+        expect(clamp(7,-4,4)).to.be.a('number');
+        expect(clamp(7,-4,4)).to.equal(4);
     });
-    it('input numbers 6,-6 and -8, result should be number and equal to -6', () => {
-        expect(clamp(6,-6,-8)).to.be.a('number');
-        expect(clamp(6,-6,-8)).to.equal(-6);
+    it('input numbers -8,-6 and 6, result should be number and equal to -6', () => {
+        expect(clamp(-8,-6,6)).to.be.a('number');
+        expect(clamp(-8,-6,6)).to.equal(-6);
     });
     it('input numbers 3,-3 and -2, result should be number and equal to -2', () => {
-        expect(clamp(3,-3,-2)).to.be.a('number');
-        expect(clamp(3,-3,-2)).to.equal(-3);
+        expect(clamp(-2,-3,3)).to.be.a('number');
+        expect(clamp(-2,-3,3)).to.equal(-2);
     });
 });
