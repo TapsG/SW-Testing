d7aeeeb91d4e723f52b41be78d8e8a6bfa6e2511
TapsG <50074292+TapsG@users.noreply.github.com>
2021-12-02 11:23:22 +0200

    Update clamp.test.js

--- clamp.test.js
+++ clamp.test.js
@@ -11,7 +11,7 @@ describe('clamp()', () => {
         expect(clamp(-8,-6,6)).to.be.a('number');
         expect(clamp(-8,-6,6)).to.equal(-6);
     });
-    it('input numbers 3,-3 and -2, result should be number and equal to -2', () => {
+    it('input numbers -2,-3 and 3, result should be number and equal to -2', () => {
         expect(clamp(-2,-3,3)).to.be.a('number');
         expect(clamp(-2,-3,3)).to.equal(-2);
     });
