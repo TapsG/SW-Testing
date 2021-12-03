d67ccb2a6fe4966a4f547145ba644c32f4e98945
laril <lari.lantonen@tuni.fi>
2021-12-03 16:43:34 +0200

    clamp and camelCase has bugs

--- camelCase.test.js
+++ camelCase.test.js
@@ -3,8 +3,8 @@ import camelCase from './src/camelCase.js';
 var expect = chai.expect
 
 describe('camelCase()', () => {
-    it('input "Kebab on hyvaa", result should be string and equal to " kebabOnHyvaa"', () => {
+    it('input "Kebab on hyvaa", result should be string and equal to "kebabOnHyvaa"', () => {
         expect(camelCase("Kebab on hyvaa")).to.be.string;
-        expect(camelCase("Kebab on hyvaa")).to.equal(" kebabOnHyvaa");
+        expect(camelCase("Kebab on hyvaa")).to.equal("kebabOnHyvaa");
     });
 });
--- clamp.test.js
+++ clamp.test.js
@@ -7,12 +7,21 @@ describe('clamp()', () => {
         expect(clamp(7, -4, 4)).to.be.a('number');
         expect(clamp(7, -4, 4)).to.equal(4);
     });
+    it('input numbers 7, 4 and -4, result should be number and equal to 4', () => {
+        expect(clamp(7, 4, -4)).to.be.a('number');
+        expect(clamp(7, 4, -4)).to.equal(4);
+    });
     it('input numbers -8, -6 and 6, result should be number and equal to -6', () => {
         expect(clamp(-8, -6, 6)).to.be.a('number');
         expect(clamp(-8, -6, 6)).to.equal(-6);
+    });
+    it('input numbers -8, 6 and -6, result should be number and equal to -6', () => {
+        expect(clamp(-8, 6, -6)).to.be.a('number');
+        expect(clamp(-8, 6, -6)).to.equal(-6);
     });
     it('input numbers -2, -3 and 3, result should be number and equal to -3', () => {
         expect(clamp(-2, -3, 3)).to.be.a('number');
         expect(clamp(-2, -3, 3)).to.equal(-3);
     });
+
 });
